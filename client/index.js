a4c8630162aece50dd08e601c90af7a66ba69a98
Evgeniy <evgeniy.heywait@gmail.com>
2016-08-02 01:47:07 +0300

    added redux

--- client/index.js
+++ client/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import * as reducers from './reducers';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
