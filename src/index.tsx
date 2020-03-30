cb8b3188ae30b7df02633429f7708a63a64a0fcb
heywait <evgeniy.heywait@gmail.com>
2020-03-31 00:06:27 +0300

    moved Application.tsx; added reset.css;

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Application from '../application/Application';
+import Application from './components/application/Application';
 
 ReactDOM.render(<Application />, document.getElementById('root'));
